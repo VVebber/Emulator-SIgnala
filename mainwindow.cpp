111c78999cd291f7accfd6b0f865d098c7ddd4aa
vvebber <malihevdanil@yandex.ru>
2024-08-29 00:42:42 +0300

    end?

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,18 +6,18 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    /*Соединение*/
     socket = new QTcpSocket;
     connect(socket, &QTcpSocket::readyRead, this, &MainWindow::ReadToClient);
     connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
-
+    /*Система кординат*/
     scene = new QGraphicsScene(this);           //Создание окла
     scene->setSceneRect(-100, -100, 200, 200);  //Размеры 200x200
     scene->addLine(-100,0,100,0);               //Линия для x
     scene->addLine(0,-100,0,100);               //Линия для y
-    Waves = new QPainterPath();
-
     ui->WeveDisplay->setScene(scene);
-
+    /*Волны*/
+    Waves = new QPainterPath();
     pathWaves = new QGraphicsPathItem;
     scene->addItem(pathWaves);
 }
@@ -63,18 +63,21 @@ void MainWindow::ReadToClient(){
     if(in.status() == QDataStream::Ok){
         QPoint Point;
         in >> Point;
-
+        //*Волны, установка начальной позиции
         if(Waves->elementCount() == 0){
             Waves->moveTo(Point);
+        }// Удаление, волня(когда полностью отрисована)
+        else if(Waves->elementCount() == 200){
+            Waves->clear();
         }
         else
             Waves->lineTo(Point);
         pathWaves->setPath(*Waves);
 
+        qDebug() <<"Size"<<Waves->elementCount();
+
         QTextStream WriteSignalData(SignalData);
         WriteSignalData<<Point.x() <<":"<<Point.y()<<"\n";
-
-        qDebug() <<"size" << Waves->elementCount();
     } else
         qDebug() <<"Data error";
 }
@@ -95,7 +98,7 @@ void MainWindow::SendToClient() {
 void MainWindow::on_ConnectToServer_clicked()
 {
     socket->connectToHost(ui->lineAddres->text(), ui->spinPort->value());
-    if (socket->waitForConnected(3000)) {  // Ожидаем подключения в течение 3 секунд
+    if (socket->waitForConnected(3000)) {//3 секунды
         qDebug() << "Подключение установлено";
         QMessageBox::about(this, "Status", "Полключено");
     } else {
@@ -114,21 +117,18 @@ void MainWindow::on_SendRequest_clicked()
                 QMessageBox::critical(0, "Файл не открыт", "Невозможно открыть файл для записи: " + SignalData->errorString());
         }
 
-        if(Waves->elementCount() == 0 || Waves->elementCount() >= 200){//=>200, иногда размер 200 или 201
-            QTextStream WriteSignalData(SignalData);
-            WriteSignalData<<"TypeSignal: " << ui->TypeSignal->currentText()<<"\n";
+        QTextStream WriteSignalData(SignalData);
+        WriteSignalData<<"TypeSignal: " << ui->TypeSignal->currentText()<<"\n";
+        /*Волны*/
+        scene->removeItem(pathWaves);
+        Waves->clear();
+        delete Waves;
+        Waves = new QPainterPath();
+        pathWaves->setPath(*Waves);
+        scene->addItem(pathWaves);
 
-            scene->removeItem(pathWaves);  // Удаление волны из сцены
-
-            Waves->clear();                // Отчиска волдны
-            delete Waves;
-            Waves = new QPainterPath();
-
-            pathWaves->setPath(*Waves);    // Добавление волны в объект для отрисовки
-            scene->addItem(pathWaves);     // Добавление объекта
-
-            SendToClient();                // Отправка сообщения
-        } else ui->label->setText("Подождите, процесс не завершен");
+        qDebug() << "press";
+        SendToClient();
     } else ui->label->setText("нес соединения");
 }
 
