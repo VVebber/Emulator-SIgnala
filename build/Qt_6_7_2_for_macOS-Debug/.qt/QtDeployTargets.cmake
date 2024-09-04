72d145cef285605dfe899ef3ca9f3ec4efaa4766
vvebber <malihevdanil@yandex.ru>
2024-09-04 16:52:07 +0300

    RemoveFile1.2

--- build/Qt_6_7_2_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Client_FILE /Users/malysevdanil/Documents/Projects/P2/Client/build/Qt_6_7_2_for_macOS-Debug/Client.app/Contents/MacOS/Client)
-set(__QT_DEPLOY_TARGET_Client_TYPE EXECUTABLE)
